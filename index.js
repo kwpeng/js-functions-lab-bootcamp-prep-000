162482cc02e2a6790ee83430d5a41dd28d9736ce
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-05-07 11:58:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,11 +9,3 @@ function happyHolidaysTo(name) {
   return `"Happy holiday," + ${name} + "!"`
   console.log happyHolidaysTo()
 }
-
-function happyHolidayTo(holiday, name) {
-  return `"Happy ${holiday} + ${name} +" !"`
-}
-
-function holidayCountdown(holiday, nakme) {
-  return `"It's + ${holiday} + ${name} +"!"`
-}
